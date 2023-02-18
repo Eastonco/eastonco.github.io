683c4522d06e5da074a523b3b0f923d95b70fdf7
Connor Easton <eastonco@icloud.com>
2023-02-18 02:25:10 -0800

    feat: overhaul wip

--- src/app/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.scss";
-import InProgress from "../components/InProgress";
-import { Loading } from "../components/Loading";
-
-function App() {
-  return <Loading />;
-}
-
-export default App;
---
+++ src/app/App.tsx
@@ -0,0 +1,31 @@
+import { Home } from "../components/home-page/Home";
+import './app.scss';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD17dp_uneNLE5fPcYR6w2b-e2_fCTYcIo",
+  authDomain: "website-b1e61.firebaseapp.com",
+  projectId: "website-b1e61",
+  storageBucket: "website-b1e61.appspot.com",
+  messagingSenderId: "801623926680",
+  appId: "1:801623926680:web:b9bce06ba4caecd5186ded",
+  measurementId: "G-LLWGGT0Z4W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+logEvent(analytics, 'master_view');
+
+function App() {
+  return <Home />;
+}
+
+export default App;
