48b02ad7edf3b6b0e07aedd089ad509b8b5be56e
Connor Easton <eastonco@icloud.com>
2026-02-22 00:38:39 -0800

    chore: node v20, next v16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -9,12 +11,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  ...compat.extends('prettier'),
-  {
-    ignores: ['node_modules/', '.next/', 'out/', 'public/'],
-  },
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.extends("prettier"), {
+  ignores: ['node_modules/', '.next/', 'out/', 'public/'],
+}];
 
 export default eslintConfig;
